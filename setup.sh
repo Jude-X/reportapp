9a7a967f4672370668ddf559e8a4f9c54d4ee666
Jude-X <judexonyekaba@gmail.com>
2020-12-30 21:50:25 +0100

    added bcrypt for security

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.streamlit/
 echo "[general]
-email = \"judexstreamlitreportapp.herokuapp.com\"
+email = \"judexonyekaba@gmail.com\"
 " > ~/.streamlit/credentials.toml
 echo "[server]
 headless = true
